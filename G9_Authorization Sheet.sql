4c436fcb3e7d0a5ba3f3293ce5f60c3c6c1fb7f2
Zongyao Li <li.zongyao@northeastern.edu>
2023-03-23 19:57:09 -0400

    Finished the automatic script

---
+++ G9_Authorization Sheet.sql
@@ -0,0 +1,11 @@
+show user;
+
+grant all on dealer to ming, zongyao, fangyu;
+grant all on customer to ming, zongyao, fangyu;
+grant all on reviews to ming, zongyao, fangyu;
+grant all on connections to ming, zongyao, fangyu;
+grant all on favorites to ming, zongyao, fangyu;
+grant all on manufacturer to ming, zongyao, fangyu;
+grant all on car_model to ming, zongyao, fangyu;
+grant all on features to ming, zongyao, fangyu;
+grant all on inventory to ming, zongyao, fangyu;
--- G9_ZONGYAO.sql
+++ G9_ZONGYAO.sql
@@ -5,3 +5,24 @@ create table TEST (
     );
 
 grant all on TEST to FANGYU, MING;
+
+select * from g9.dealer;
+select * from g9.customer;
+select * from g9.reviews;
+select * from g9.favorites;
+select * from g9.inventory;
+select * from g9.car_make;
+select * from g9.connections;
+select * from g9.features;
+select * from g9.manufacturer;
+
+CREATE SEQUENCE manufacturer_id
+START WITH 1
+INCREMENT BY 1;
+
+INSERT INTO manufacturer (fid, make_name, country, descript, year_founded) VALUES (manufacturer_id.nextval, 'Ford', 'USA', 'One of the oldest founders', '1903');
+INSERT INTO manufacturer (fid, make_name, country, descript, year_founded) VALUES (manufacturer_id.nextval, 'GMC', 'USA', 'American heavy duty vehicle manufacurer', '1911');
+INSERT INTO manufacturer (fid, make_name, country, descript, year_founded) VALUES (manufacturer_id.nextval, 'Audi', 'Germany', 'German luxry car brand', '1909');
+INSERT INTO manufacturer (fid, make_name, country, descript, year_founded) VALUES (manufacturer_id.nextval, 'BMW', 'Germany', 'German luxry car brand', '1916');
+commit;
+
