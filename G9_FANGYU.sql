bb8a7c0b80957c67c9b23d246b69b16380551304
fangyu wu <98557748+EasonWu7@users.noreply.github.com>
2023-03-23 16:35:55 -0400

    Add files via upload

---
+++ G9_FANGYU.sql
@@ -0,0 +1,10 @@
+
+insert into g9.Reviews values(10001,1,2345671,'i like it',to_date('2018-03-12','yyyy-mm-dd'));
+insert into g9.Reviews values(10002,2,2345672,'i love the car so much',to_date('2022-04-12','yyyy-mm-dd'));
+insert into g9.Reviews values(10003,3,2345673,'everything is great',to_date('2021-03-22','yyyy-mm-dd'));
+commit;
+
+insert into g9.favorites values(100007,1,'LGWEFSEE3DFA333F2',to_date('2019-03-08','yyyy-mm-dd'));
+insert into g9.favorites values(100008,2,'LGWSSSDE3DFAWS3SS',to_date('2020-04-11','yyyy-mm-dd'));
+insert into g9.favorites values(100009,3,'LVWEDDSEE3EFA534F',to_date('2019-05-10','yyyy-mm-dd'));
+commit;
